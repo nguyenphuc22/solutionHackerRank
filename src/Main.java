c7d78dbed3eb15d6477c419a728f669ff5ca090a
phuc <nguyenphuc220100@gmail.com>
2020-01-21 20:52:19 +0700

    Sock Merchant

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("/home/phuc/IdeaProjects/solutionHackerRank/src/data.txt"));
+            String number = bufferedReader.readLine();
+            String value = bufferedReader.readLine();
+            String[] arrayValue;
+            arrayValue = value.split(" ");
+            int[] ar = new int[Integer.parseInt(number)];
+            for (int i = 0 ; i < ar.length;i++)
+            {
+                ar[i] = Integer.parseInt(arrayValue[i]);
+            }
+            int count = Array.sockMerchant(Integer.parseInt(number),ar);
+            System.out.println(count);
+            bufferedReader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
