91717eab953510c25f6210a17f0ec80d55fb20de
PhucVR <59957741+nguyenphuc22@users.noreply.github.com>
2024-06-09 08:50:28 +0700

    Refactor workspace.xml and Solution.java, and update Solution class in SubarrayDivision problem.

--- src/SubarrayDivision/Solution.java
+++ src/SubarrayDivision/Solution.java
@@ -1,3 +1,5 @@
+package SubarrayDivision;
+
 import java.io.*;
 import java.math.*;
 import java.security.*;
@@ -24,34 +26,45 @@ class Result {
 
     public static int birthday(List<Integer> s, int d, int m) {
         // Write your code here
-
+        int count = 0;
+        for (int i = 0; i < s.size() - m + 1; i++) {
+            int sum = 0;
+            for (int j = i; j < i + m; j++) {
+                sum += s.get(j);
+            }
+            if (sum == d) {
+                count++;
+            }
+        }
+        return count;
     }
 
 }
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+//
+//        int n = Integer.parseInt(bufferedReader.readLine().trim());
+//
+//        List<Integer> s = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+//                .map(Integer::parseInt)
+//                .collect(toList());
+//
+//        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+//
+//        int d = Integer.parseInt(firstMultipleInput[0]);
+//
+//        int m = Integer.parseInt(firstMultipleInput[1]);
 
-        int n = Integer.parseInt(bufferedReader.readLine().trim());
+        int result = Result.birthday(Arrays.asList(4), 4, 1);
 
-        List<Integer> s = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-                .map(Integer::parseInt)
-                .collect(toList());
-
-        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
-
-        int d = Integer.parseInt(firstMultipleInput[0]);
-
-        int m = Integer.parseInt(firstMultipleInput[1]);
-
-        int result = Result.birthday(s, d, m);
-
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
-
-        bufferedReader.close();
-        bufferedWriter.close();
+        System.out.println(result);
+//        bufferedWriter.write(String.valueOf(result));
+//        bufferedWriter.newLine();
+//
+//        bufferedReader.close();
+//        bufferedWriter.close();
     }
 }
